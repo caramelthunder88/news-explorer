7767d84ad950ad5d90e330b013168cfcb50f4ef9
Donte <damorgan19@gmail.com>
2025-09-14 10:09:25 -0400

    chore: set up project infrastructure (vite scaffold, folders, components, utils, pages)

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>News Explorer</h1>
+      <p>If you see this, your Vite setup is working ✅</p>
+    </div>
+  );
+}
+
+export default App;
